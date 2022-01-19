29885b56df64cb1b3d6fdc9054f69dd9443a697d
whaples0023 <97602450+whaples0023@users.noreply.github.com>
2022-01-19 07:07:54 -0500

    Mallori Whaples submission 3 Task 01 upload b

--- Task_01/task01.r
+++ Task_01/task01.r
@@ -3,3 +3,33 @@ library(swirl)
 library (swirl)
 swirl()
 swirl()
+library (swirl)
+swirl()
+wwooloful
+1
+1
+1
+1
+5+7
+x <- 5+7
+x
+y<-x-3
+y
+z <-c(1.1,9,3.14)
+c(1.1,9,3.14)
+z <- c(1.1,9,3.14)
+?c
+z
+c(z, 555, z)
+z * 2 + 100
+z - 1^2
+my_sqrt <- sqrt(z - 1)
+2
+3
+my_sqrt
+my_div <- z/my_sqrt
+1
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
