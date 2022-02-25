71ece6e9af5524dc4ca31561afa7531b175ece22
whaples0023 <97602450+whaples0023@users.noreply.github.com>
2022-02-25 10:52:27 -0500

    Mallori Whaples Project

--- Project Folder/Project.R
+++ Project Folder/Project.R
@@ -1,3 +1,4 @@
 install.packages(type="rfigshare")
 setwd('C:\\Users\\Mallori\\OneDrive\\Desktop\\Evolution\\Tasks\\Project Folder')
-Data <- read.csv('https://figshare.com/articles/dataset/Compensatory_effect_of_egg_size_csv/14798352/1')
+Data <- read.csv('DatasheetA.csv')
+head(Data)
