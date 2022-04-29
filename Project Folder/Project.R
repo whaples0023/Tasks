802a6969738a1b9526ceec71329a18fadbfc766a
whaples0023 <97602450+whaples0023@users.noreply.github.com>
2022-04-29 09:41:59 -0400

    Task 11

--- Project Folder/Project.R
+++ Project Folder/Project.R
@@ -1,7 +1,7 @@
 setwd('C:\\Users\\Mallori\\OneDrive\\Desktop\\Evolution\\Tasks\\Project Folder')
 Data2<-read.csv('BlackRObinData2.csv')
 head(Data2)
-plot(Data2$ll, Data2$Fa, xlab="Recessive Allele Frequency in Offspring", ylab="Ancestoral Inbreeding Frequency", pch=16)
-one.way <- aov(ll ~ Fa, data = Data2)
+plot(Data2$ll, Data2$Fa, xlab="Recessive Allele Frequency in Offspring", ylab="Recessive Allele Frequency in Ancestors", pch=16)
 abline(lm(Data2$Fa~Data2$ll)) 
-summary(one.way)cd
+cor.test(Data2$ll,Data2$Fa)
+         
